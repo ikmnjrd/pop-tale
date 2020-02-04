37e32b46396e11137adf48a7d1a1efc7e4be1d61
ikmnjrd <ike_mba@IkedanoMacBook-Air.local>
2020-02-05 04:20:02 +0900

    carrierwaveの初期対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_130356) do
+ActiveRecord::Schema.define(version: 2020_01_31_143303) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_130356) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
