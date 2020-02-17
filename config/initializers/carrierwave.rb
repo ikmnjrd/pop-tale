a91381d5c477f7c8c2299bc1aaf14aff39b63cc1
ikmnjrd <ike_mba@IkedanoMacBook-Air.local>
2020-02-17 16:37:23 +0900

    AWSアクセスキーを環境変数へ対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: 'AKIAV6DFY5WZ5C4HB5HF',
-        aws_secret_access_key: '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',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         region: 'ap-northeast-1'
       }
   
