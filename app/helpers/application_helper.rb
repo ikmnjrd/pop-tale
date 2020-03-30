49d294227d788cece4f1d44e657e660d5eafe4f5
ikmnjrd <ikmonjara2rnmt@gmail.com>
2020-03-30 18:29:03 +0900

    bugfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "PopTale"
+    base_title = "aiilust"
     if page_title.empty?
     base_title
     else
